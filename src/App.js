ee9a551ea4eec89fc68e4171813eb5f9e63aa7eb
Hope-Ray <90702054+Hope-Ray@users.noreply.github.com>
2022-01-09 14:31:57 +0530

    initiial stage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { FormControl, MenuItem, Select } from '@material-ui/core';
+import './App.css'
 
 
 function App() {
   return (
-    <div className="App">
-      Hello testing 2
+    <div className="app">
+      <div className='app__header'>
+      <h1>COVID-19 TRACKER</h1>
+      <FormControl className='app__dropdown'>
+        <Select 
+          variant='outlined'
+          value={'abc'}
+        >
+          <MenuItem value='worldwide'>Worldwide</MenuItem>
+          <MenuItem value='worldwide'>option 2</MenuItem>
+          <MenuItem value='worldwide'>option 3</MenuItem>
+          <MenuItem value='worldwide'>option 4</MenuItem>
+        </Select>
+      </FormControl>
+
+      </div>
     </div>
   );
 }
