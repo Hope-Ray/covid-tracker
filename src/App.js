775031ae768494ae3768f7434fbbf6d77b651ea9
Hope-Ray <90702054+Hope-Ray@users.noreply.github.com>
2022-01-11 11:39:59 +0530

    Stage 1 basic layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import { FormControl, MenuItem, Select } from '@material-ui/core';
+import React, {useState, useEffect} from 'react';
+import { Card, CardContent, FormControl, MenuItem, Select } from '@material-ui/core';
 import './App.css'
+import InfoBox from './InfoBox';
+import Map from './Map';
+import Table from './Table';
 
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide');
+  const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([])
+
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+    .then((response) => response.json())
+    .then(data => {
+      setCountryInfo(data);
+    })
+  }, [])
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+    await  fetch("https://disease.sh/v3/covid-19/countries")
+    .then((response) => response.json())
+    .then((data) => {
+        const countries = data.map((country) => (
+          {
+            name: country.country, //United States, United Kingdom
+            value: country.countryInfo.iso2 //Uk, USA
+          }
+        ));
+        setTableData(data);
+        setCountries(countries);
+    })
+    }
+    getCountriesData();
+  }, []);
+
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+
+    const url =
+      countryCode === "worldwide"
+        ? "https://disease.sh/v3/covid-19/all"
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+
+    await fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      setCountry(countryCode);
+
+      // all of the country data
+      setCountryInfo(data);
+    })
+    console.log(countryInfo);
+
+  } 
+
   return (
+    // Header
     <div className="app">
-      <div className='app__header'>
-      <h1>COVID-19 TRACKER</h1>
-      <FormControl className='app__dropdown'>
-        <Select 
-          variant='outlined'
-          value={'abc'}
-        >
-          <MenuItem value='worldwide'>Worldwide</MenuItem>
-          <MenuItem value='worldwide'>option 2</MenuItem>
-          <MenuItem value='worldwide'>option 3</MenuItem>
-          <MenuItem value='worldwide'>option 4</MenuItem>
-        </Select>
-      </FormControl>
+      <div className='app__left'>
+        {/* Header */}
+        <div className='app__header'>
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className='app__dropdown'>
+          <Select 
+            variant='outlined'
+            value={country}
+            onChange={onCountryChange}
+          >
+            <MenuItem value='worldwide'>Worldwide</MenuItem>
+            {countries.map((country) => <MenuItem value={country.value}>{country.name}</MenuItem>)}
+          </Select>
+        </FormControl>
+        </div>
+
+        {/* InfoBox */}
+        <div className='app__stats'>
+          <InfoBox title={"Coronavirus Cases"} cases={countryInfo.todayCases} total={countryInfo.cases}></InfoBox>
+
+          <InfoBox title={"Recovered"} cases={countryInfo.todayRecovered} total={countryInfo.recovered}></InfoBox>
+
+          <InfoBox title={"Deaths"} cases={countryInfo.todayDeaths} total={countryInfo.deaths}></InfoBox>
+        </div>
+
+        <Map/>
 
       </div>
+      <Card className='app__right'>
+        <CardContent>
+          <h3>Live Cases by Country</h3>
+          <Table countries={tableData} />
+          <h3>Worldwide new cases</h3>
+          {/* Graph */}
+        </CardContent>
+      </Card>
+
     </div>
   );
 }
---
+++ src/InfoBox.js
@@ -0,0 +1,24 @@
+import { Card, CardContent, Typography } from '@material-ui/core';
+import React from 'react'
+
+function InfoBox({ title, cases, total }) {
+   return (
+      <Card className="infoBox">
+         <CardContent>
+            {/* Titlee */}
+            <Typography className='infoBox__title' color='textSecondary'>
+               {title}
+            </Typography>
+
+            {/* Number of Cases */}
+            <h2 className="infobox__cases">{cases}</h2>
+            {/* Total cases */}
+            <Typography className='infoBox__total' color='textSecondary'>
+               {total} Total
+            </Typography>
+         </CardContent>
+      </Card>
+   )
+}
+
+export default InfoBox;
