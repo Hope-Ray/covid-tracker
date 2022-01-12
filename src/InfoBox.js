7d6a97d7c46552780ccaf97483241a22ed599f73
Hope-Ray <90702054+Hope-Ray@users.noreply.github.com>
2022-01-12 22:12:03 +0530

    check

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -1,24 +1,30 @@
-import { Card, CardContent, Typography } from '@material-ui/core';
-import React from 'react'
+import React from "react";
+import { Card, CardContent, Typography } from "@material-ui/core";
+import "./InfoBox.css";
 
-function InfoBox({ title, cases, total }) {
-   return (
-      <Card className="infoBox">
-         <CardContent>
-            {/* Titlee */}
-            <Typography className='infoBox__title' color='textSecondary'>
-               {title}
-            </Typography>
+function InfoBox({ title, cases, total, active, isRed, ...props }) {
+  console.log(title, active);
+  return (
+    <Card
+      onClick={props.onClick}
+      className={`infoBox ${active && "infoBox--selected"} ${
+        isRed && "infoBox--red"
+      }`}
+    >
+      <CardContent>
+        <Typography color="textSecondary" gutterBottom>
+          {title}
+        </Typography>
+        <h2 className={`infoBox__cases ${!isRed && "infoBox__cases--green"}`}>
+          {cases}
+        </h2>
 
-            {/* Number of Cases */}
-            <h2 className="infobox__cases">{cases}</h2>
-            {/* Total cases */}
-            <Typography className='infoBox__total' color='textSecondary'>
-               {total} Total
-            </Typography>
-         </CardContent>
-      </Card>
-   )
+        <Typography className="infoBox__total" color="textSecondary">
+          {total} Total
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 }
 
 export default InfoBox;
